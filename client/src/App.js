59435d74b4482e0c8e72d3f5e924602931e98f9b
8ko <8ko.exe@gmail.com>
2022-05-17 00:03:46 +0800

    swal changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 import Swal from 'sweetalert2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [password, setPassword] = useState("");
@@ -16,26 +18,66 @@ function App() {
   }, []);
 
   const addPassword = () => {
-    Axios.post("http://localhost:3001/addpassword", {
-      password: password,
-      title: title,
-    });
-    console.log("Password added");
-    document.getElementById('title').value = '';
-    document.getElementById('password').value = '';
+    var title=document.getElementById('title').value;
+    var password=document.getElementById('password').value;
+    if (title!=='' || password!==''){
+      console.log("hello");
+      Axios.post("http://localhost:3001/addpassword", {
+        password: password,
+        title: title,
+      });
+      console.log("Password added");
+      // title = '';
+      // password = '';
+      
+      Swal.fire({
+        title: 'Success!',
+        text: 'Password has been added to your vault.',
+        icon: 'success',
+        // showConfirmButton: false,
+        confirmButtonColor: '#318ce7',
+        confirmButtonText: 'Okay',
+        showCloseButton: true,
+        closeButtonHtml: '&times;',
+        timer: 5000
+      }).then((result) =>{
+        window.location.reload();
+      });
+
+    } else {
+      console.log('empty');
+      Swal.fire({
+        title: 'Error!',
+        text: 'Please fill out the fields.',
+        icon: 'error',
+        showConfirmButton: false,
+        showCloseButton: true,
+        closeButtonHtml: '&times;',
+        timer: 1500
+      });
+    }
     
-    Swal.fire({
-      title: 'Success!',
-      text: 'Password has been added to your vault.',
-      icon: 'success',
-      confirmButtonColor: '#318ce7',
-      confirmButtonText: 'Okay',
-      showCloseButton: 'true',
-      closeButtonHtml: '&times;',
-    }).then((result) =>{
-      window.location.reload();
-    });
-    // window.location.reload();
+  };
+
+  const deletePassword = () => {
+    Axios.delete()
+    // Axios.post("http://localhost:3001/deletepassword", {
+    //   password: password,
+    //   title: title,
+    // });
+    console.log("Password deleted");
+
+    // Swal.fire({
+    //   title: 'Success!',
+    //   text: 'Password has been removed from your vault.',
+    //   icon: 'success',
+    //   confirmButtonColor: '#318ce7',
+    //   confirmButtonText: 'Okay',
+    //   showCloseButton: 'true',
+    //   closeButtonHtml: '&times;',
+    // }).then((result) =>{
+    //   window.location.reload();
+    // });
   };
 
   const decryptPassword = (encryption) => {
@@ -72,32 +114,47 @@ function App() {
             setPassword(event.target.value);
           }}
         />
-        <input
+        <input 
           type="text"
           id="title"
           placeholder="Ex. Facebook"
           onChange={(event) => {
             setTitle(event.target.value);
-          }}
+          }}z
         />
-        <button onClick={addPassword}> Add Password</button>
-      </div>
+        <button onClick={addPassword}> Add Password </button></div>
 
-      <div className="Passwords">
+      <div className="Passwords w-50 d-flex">
         {passwordList.map((val, key) => {
           return (
-            <div
-              className="password"
-              onClick={() => {
-                decryptPassword({
-                  password: val.password,
-                  iv: val.iv,
-                  id: val.id,
-                });
-              }}
-              key={key}
-            >
-              <h3>{val.title}</h3>
+            <div className="container-fluid">
+              <div className="row text-white ">
+                <div className="col-8">
+                  <div
+                    className="password w-100 h-75 rounded"
+                    onClick={() => {
+                      decryptPassword({
+                        password: val.password,
+                        iv: val.iv,
+                        id: val.id,
+                      });
+                    }}
+                    key={key}
+                  >
+                    <h3>{val.title}</h3> 
+                  </div>
+                </div>
+                <div className="col w-100">
+                  <div className="view h-75 rounded">
+                    <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                  </div>
+                </div>
+                <div className="col w-100">
+                  <div className="del h-75 rounded">
+                    <FontAwesomeIcon onClick={deletePassword} icon={faTrash}></FontAwesomeIcon>
+                  </div>
+                </div>
+              </div>
             </div>
           );
         })}
