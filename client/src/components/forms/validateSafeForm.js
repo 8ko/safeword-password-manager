9ee152614114e587ba9c5d26b4fc2ffd436af7f4
8ko <8ko.exe@gmail.com>
2022-05-21 10:33:36 +0800

    added only spaces validation

--- client/src/components/forms/validateSafeForm.js
+++ client/src/components/forms/validateSafeForm.js
@@ -1,26 +1,31 @@
 import { VaultItemTypes } from "../../constants";
 
+// check if input is empty or contains only spaces
+function validate(str) {
+    return !(!str || /^\s*$/.test(str));
+}
+
 export default function validateSafeForm(values, type) {
     let errors = {};
 
     if (type === VaultItemTypes.Login) {
 
-        if (!values.title) errors.title = "* Title is required";
-        if (!values.password) errors.password = "* Password is required";
+        if (!validate(values.title)) errors.title = "* Title is required";
+        if (!validate(values.password)) errors.password = "* Password is required";
 
     } else if (type === VaultItemTypes.Card) {
 
-        if (!values.title) errors.title = "* Brand is required";
-        if (!values.name) errors.name = "* Name is required";
-        if (!values.number) errors.number = "* Card number is required";
-        if (!values.month) errors.year = "* Expiry month is required";
-        if (!values.year) errors.year = "* Expiry year is required";
-        if (!values.cvv) errors.cvv = "* CVV/CVC is required";
+        if (!validate(values.title)) errors.title = "* Brand is required";
+        if (!validate(values.name)) errors.name = "* Name is required";
+        if (!validate(values.number)) errors.number = "* Card number is required";
+        if (!validate(values.month)) errors.year = "* Expiry month is required";
+        if (!validate(values.year)) errors.year = "* Expiry year is required";
+        if (!validate(values.cvv)) errors.cvv = "* CVV/CVC is required";
 
     } else if (type === VaultItemTypes.Note) {
 
-        if (!values.title) errors.title = "* Title is required";
-        if (!values.note) errors.note = "* Note is required";
+        if (!validate(values.title)) errors.title = "* Title is required";
+        if (!validate(values.note)) errors.note = "* Note is required";
     }
 
     return errors;
