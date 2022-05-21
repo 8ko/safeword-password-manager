6b8bd0f70bcac727261d7d955afc4328c797af38
8ko <8ko.exe@gmail.com>
2022-05-21 10:32:28 +0800

    added app logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import * as React from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--- client/src/components/appbarguest.jsx
+++ client/src/components/appbarguest.jsx
@@ -1,25 +1,66 @@
-import React from 'react';
+import React from 'react'; 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
+const pwet = [
+  "u is gay",
+  "i am gay",
+  "eat my ass",
+  "suck fat cock",
+  "eat my shit",
+  "i love booba",
+  "big pp",
+  "show bobs",
+  "show vegana",
+  "bata pa ko kol",
+  "sakit kaayo kol",
+  "kol bata pa ko kol",
+  "imong pante"
+];
 
 const AppbarGuest = () => {
-    return (
-        <>
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, alignItems: 'center' }}>
-            <Toolbar>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ mr: 2 }}
-              >
-                Safe&nbsp;<mark>Word</mark>
-              </Typography>
-            </Toolbar>
+  return (
+    <>
+      <Toolbar>
+        <AppBar enableColorOnDark
+          position="fixed"
+          sx={{
+            flexGrow: 1,
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            px: 1
+          }}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+          >
+            <IconButton
+              size="large"
+              color="inherit"
+              aria-label="menu"
+            >
+              <img src={process.env.PUBLIC_URL + "/SafeLogo34Full.png"} alt="SafeLogo" />
+            </IconButton>
+
+            <IconButton
+              size="large"
+              color="inherit"
+              aria-label="menu"
+            >
+              <Tooltip title={pwet[parseInt(Math.random() * pwet.length)]} placement="bottom">
+                <InfoIcon />
+              </Tooltip>
+            </IconButton>
+          </Stack>
         </AppBar>
-        </>
-    );
+      </Toolbar>
+    </>
+  );
 };
 
 export default AppbarGuest;
