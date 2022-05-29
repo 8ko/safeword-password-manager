9b8a8eaa598aacdf164e151caa92e4dee2641b35
Charles Mendoza <franztopacio9@gmail.com>
2022-05-30 05:27:38 +0800

    updated env variables

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL
+    baseURL: BASE_URL
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL,
+    baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
--- server/EncryptionHandler.js
+++ server/EncryptionHandler.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const secret = process.env.SECRET_KEY;
+const secret = process.env.AES_SECRET_KEY; // require('crypto').randomBytes(16).toString('hex')
 
 const encrypt = (password) => {
   const iv = Buffer.from(crypto.randomBytes(16));
